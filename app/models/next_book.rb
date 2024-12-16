e9ed2d80465d3d0320e22762b6df9dfcb1c2a598
Hollicint <hollyjdowling@gmail.com>
2024-12-16 00:23:55 +0000

    Unit Test for NextBook Page

--- app/models/next_book.rb
+++ app/models/next_book.rb
@@ -1,2 +1,6 @@
 class NextBook < ApplicationRecord
+    validates  :title, presence: true
+    validates  :author, presence: true
+    validates  :status, presence: true
+    validates  :note, presence: true
 end
--- test/models/next_book_test.rb
+++ test/models/next_book_test.rb
@@ -4,4 +4,14 @@ class NextBookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Next Book shouldn't be added without form fully filled in" do
+    nextBook = NextBook.new
+    assert_not nextBook.save, "not save book without form complete"
+
+end  
+
+test "Next Book should be added when form is fully filled " do
+  nextBook = NextBook.new(title: "Way of the house Husband", author: "Kousuke Oono", status:"Havent finished", note:"Need to buy next in series")
+    assert nextBook.save, "Save the book"
+end 
 end
